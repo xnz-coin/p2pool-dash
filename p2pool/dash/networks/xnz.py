5ad119d4dc211894038a502d8c1ffc2ce89fc350
RoarkTech <josh@digitalcake.co.nz>
2018-01-08 16:02:11 +0000

    Added XNZ network 1

---
+++ p2pool/dash/networks/xnz.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'bf0c6bbd'.decode('hex')
+P2P_PORT = 6669
+ADDRESS_VERSION = 53
+SCRIPT_ADDRESS_VERSION = 16
+RPC_PORT = 6668
+RPC_CHECK = defer.inlineCallbacks(lambda dashd: defer.returnValue(
+            'dashaddress' in (yield xnzd.rpc_help()) and
+            not (yield xnzd.rpc_getinfo())['testnet']
+        ))
+BLOCKHASH_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+BLOCK_PERIOD = 150 # s
+SYMBOL = 'XNZ'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'XnzCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/XnzCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.xnzcore'), 'xnz.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/address/'
+TX_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/tx/'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUST_THRESHOLD = 0.001e8
---
+++ p2pool/networks/xnz.py
@@ -0,0 +1,19 @@
+from p2pool.dash import networks
+
+PARENT = networks.nets['xnz']
+SHARE_PERIOD = 20 # seconds
+CHAIN_LENGTH = 24*60*60//20 # shares
+REAL_CHAIN_LENGTH = 24*60*60//20 # shares
+TARGET_LOOKBEHIND = 100 # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+SPREAD = 10 # blocks
+IDENTIFIER = '157E3F9D22A1CE6A'.decode('hex')
+PREFIX = '7C45A1F3BFF797F5'.decode('hex')
+COINBASEEXT = '0D2F5032506F6F6C2D444153482F'.decode('hex')
+P2P_PORT = 6669
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 7903
+BOOTSTRAP_ADDRS = 'seed.xnz.io '.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-xnz'
+VERSION_CHECK = lambda v: v >= 120100
